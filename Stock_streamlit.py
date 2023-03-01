501c6956189696e70c080613f7b86c2c116aad4a
shilpa720 <morisetti.shilpa@gmail.com->
2023-03-01 09:52:20 -0600

    Chnages to .py

--- Stock_streamlit.py
+++ Stock_streamlit.py
@@ -1,17 +1,17 @@
+from sklearn.preprocessing import PolynomialFeatures
 import yfinance as yf
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 import datetime as dt
 import streamlit as st
-
-
+from numpy import source
 
 # User input
 stock_symbol = st.text_input("Enter a stock symbol (e.g. AAPL):", "AAPL")
 
 # Data collection
 end_date = dt.date.today()
-start_date = end_date - dt.timedelta(days=30)
+start_date = end_date - dt.timedelta(days=720)
 df = yf.download(stock_symbol, start=start_date, end=end_date, interval='1d')
 
 # Data preprocessing
@@ -19,12 +19,28 @@ df['Day'] = range(1, len(df) + 1)
 X = df[['Day']]
 y = df['Close']
 
+
+# Model selection
+model_selection = st.selectbox("Select a model:", ("Linear Regression", "Polynomial Regression (degree 2)", "Polynomial Regression (degree 3)"))
+
+
 # Model training
-model = LinearRegression()
-model.fit(X, y)
+
+if model_selection == "Linear Regression":
+    model = LinearRegression()
+    model.fit(X, y)
+elif model_selection == "Polynomial Regression (degree 2)":
+    poly = PolynomialFeatures(degree=2)
+    X_poly = poly.fit_transform(X)
+    model = LinearRegression().fit(X_poly, y)
+elif model_selection == "Polynomial Regression (degree 3)":
+    poly = PolynomialFeatures(degree=3)
+    X_poly = poly.fit_transform(X)
+    model = LinearRegression().fit(X_poly, y)
+    
 
 # Prediction
-future_days = 30
+future_days = 720
 future_dates = pd.date_range(start=end_date, periods=future_days)
 future_df = pd.DataFrame({'Day': range(len(df) + 1, len(df) + future_days + 1)})
 future_X = future_df[['Day']]
